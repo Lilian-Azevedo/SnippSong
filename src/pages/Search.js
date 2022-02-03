3257baeeebc7848437aed8d10bc1664282e8b4c9
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-02-03 20:42:11 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    inputName: '',
+  }
+
+  handleInput = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const MIN_LETTERS = 2;
+    const { inputName } = this.state;
+
     return (
       <div data-testid="page-search">
         <h1>Search</h1>
         <Header />
+        <div>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.handleInput }
+            placeholder="Nome do artista"
+            value={ inputName }
+            name="inputName"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ inputName.length < MIN_LETTERS }
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
