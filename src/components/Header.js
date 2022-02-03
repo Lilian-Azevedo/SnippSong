911daee7f50da074ad249021086f7abf36002a62
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-02-03 20:30:48 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -23,6 +24,21 @@ export default class Header extends Component {
 
     return (
       <header data-testid="header-component">
+        <nav>
+          <ul>
+            <li><Link to="/search" data-testid="link-to-search">Pesquisar</Link></li>
+            <li>
+              <Link
+                to="/favorites"
+                data-testid="link-to-favorites"
+              >
+                Minhas músicas
+              </Link>
+            </li>
+            <li><Link to="/profile" data-testid="link-to-profile">Meu perfil</Link></li>
+          </ul>
+        </nav>
+
         {user === ''
           ? <Loading />
           : (<h2 data-testid="header-user-name">{user.name}</h2>)}
